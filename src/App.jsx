796bd10fba536320cd61193c4c0925f2e49eca45
tomasquintana-gj <tomas.quintana@gojiraf.com>
2023-05-03 10:59:46 -0300

    change name of masterPage and adds navigator bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import AuthLayout from "./pages/AuthLayout"
 import FrontPage from "./pages/FrontPage"
 import Contact from "./pages/Contact"
 import Proyects from "./pages/Proyects"
+import MainStyle from "./pages/MainStyle"
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<AuthLayout/>}>
+          <Route path='/' element={<MainStyle/>}>
             <Route index element={<FrontPage/>}/>
             <Route path='proyects' element={<Proyects/>} />
             <Route path='contact' element={<Contact/>} />
--- src/pages/MainStyle.jsx
+++ src/pages/MainStyle.jsx
@@ -4,8 +4,11 @@ const AuthLayout = () => {
 return (
     <>
       <nav className="container mt-2 flex justify-end ml-10 ">
-          <Link 
-            className="block text-center my-5 text-gray-500 px-2 hover:cursor-pointer hover:bg-gray-100 hover:rounded-3xl"
+        <Link 
+          className="block text-center my-5 text-gray-500 px-2 hover:cursor-pointer hover:bg-gray-100 hover:rounded-3xl"
+          to="/">Home Page</Link>
+        <Link 
+          className="block text-center my-5 text-gray-500 px-2 hover:cursor-pointer hover:bg-gray-100 hover:rounded-3xl"
             to="/contact">Contact</Link>
           <Link 
             className="block text-center my-5 text-gray-500 px-2 hover:cursor-pointer hover:bg-gray-100 hover:rounded-3xl"
@@ -14,6 +17,11 @@ return (
       <main className='container mx-auto md:grid md:grid-cols-2 mt-12 p-5 items-center w-8/12'>
         <Outlet />
       </main>
+      <div>
+        <h1
+          className="m-2 text-zinc-500 font-mono"
+        >@ 2023 Tomas Quintana</h1>
+      </div>
     </>
   )
 }
