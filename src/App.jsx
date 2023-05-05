a7c5cfc1e31be8bd18906bb428659b20f7966b02
tomasquintana-gj <tomas.quintana@gojiraf.com>
2023-05-05 16:19:02 -0300

    change link order

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import FrontPage from "./pages/FrontPage"
 import Contact from "./pages/Contact"
 import Proyects from "./pages/Proyects"
-import MainStyle from "./pages/MainStyle"
+import MainPage from "./pages/MainPage"
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<MainStyle/>}>
+          <Route path='/' element={<MainPage/>}>
             <Route index element={<FrontPage/>}/>
             <Route path='proyects' element={<Proyects/>} />
             <Route path='contact' element={<Contact/>} />
