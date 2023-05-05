fd23e2dc30860cabbc39967b6f8b5b2232775fff
tomasquintana-gj <tomas.quintana@gojiraf.com>
2023-05-05 16:19:37 -0300

    change name of proyects

--- src/pages/Proyects.jsx
+++ src/pages/Proyects.jsx
@@ -16,7 +16,7 @@ const Proyects = () => {
           className='text-xl'
           to='https://github.com/TomQuintana/MiloComidas'
         >
-          Servidor para Administrar Comidas
+          Meal Management Server
         </Link>
       </div>
       <div 
@@ -29,14 +29,14 @@ const Proyects = () => {
           className='text-xl'
           to='https://github.com/TomQuintana/Criptomoneda'
         >
-          Cotizador de Diferentes Criptomonedas
+          Cryptocurrency Quote        
         </Link>
       </div>   
       <div 
-        className="w-56 h-80 font-mono mb-16 border-2 border-gray-200 text-center shadow-lg hover:cursor-pointer hover:border-gray-500 hover:shadow-lg"
+        className="w-56 h-80 font-mono mb-4 border-2 border-gray-200 text-center shadow-lg hover:cursor-pointer hover:border-gray-500 hover:shadow-lg"
       >
         <img
-          className='mb-8 h-36 w-60'
+          className='mb-6 h-36 w-60'
           src={portfolio} alt="MiloComidas" />
         <Link
           className='text-xl'
