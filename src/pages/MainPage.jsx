7b4f0d964bfb15a63d9d08cb025234b18dd2fb92
tomasquintana-gj <tomquintana20@gmail.com>
2023-05-06 17:44:06 -0300

    add media query

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,7 +3,7 @@ import { Outlet, Link} from "react-router-dom"
 const AuthLayout = () => {
 return (
     <>
-      <nav className="container mt-2 flex justify-end ml-10 ">
+      <nav className="md:container mt-2 flex justify-end ml-10 ">
         <Link 
           className="block text-center my-5 text-gray-500 px-2 hover:cursor-pointer hover:bg-gray-100 hover:rounded-3xl"
           to="/">Home Page</Link>
@@ -17,7 +17,7 @@ return (
       <main className='container mx-auto md:grid md:grid-cols-2 mt-12 p-5 items-center w-8/12'>
         <Outlet />
       </main>
-      <footer className=" flex justify-start mt-16">
+      <footer className="md:flex justify-start mt-16">
         <h1
           className="m-2 text-zinc-500 font-mono"
         >@ 2023 Tomas Quintana</h1>
