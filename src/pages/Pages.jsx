adccaa6600ea5fe07571f3457d57da4a0cf7edbd
tomasquintana-gj <tomquintana20@gmail.com>
2023-05-11 16:37:56 -0300

    add media queries

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const Pages = () => {
   return (
     <>
-      <div className="w-full flex justify-end mx-4">
+      <div className="w-full flex justify-end xl:mx-4 mx-0 text-sm md:text-lg">
         <div >
           <Link 
             className="block text-center my-5 ml-2 text-gray-500 px-2 hover:cursor-pointer hover:bg-gray-100 hover:rounded-3xl"
