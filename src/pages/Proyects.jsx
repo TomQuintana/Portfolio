9bed743ca739381ee1903de81b63257598bf2071
tomasquintana-gj <tomquintana20@gmail.com>
2023-06-19 20:37:41 -0300

    ref/ remove proyects

--- src/pages/Proyects.jsx
+++ src/pages/Proyects.jsx
@@ -1,8 +1,6 @@
 import { Link } from 'react-router-dom'
 import { useTranslation } from 'react-i18next';
 import galleta from '../assets/img/galleta.jpeg'
-import logoCocch from '../assets/img/logotipo.jpeg'
-import portfolio from '../assets/img/portfolio.webp'
 import weather from '../assets/img/weather.webp'
 import blockchainLogo from '../assets/img/blockchain.png'
 
@@ -66,32 +64,7 @@ const Proyects = () => {
             </button>
           </div>
         </div>
-        <div 
-          className="w-64 h-96 font-mono mb-16 border-2 border-gray-200 text-center shadow-lg hover:border-gray-500 hover:shadow-lg items-center"
-        >
-          <img
-            className='mb-6 h-36 w-64'
-            src={portfolio} alt="MiloComidas" />
-          <Link
-            className='text-xl hover:cursor-pointer'
-            target="_blank"
-            to='https://github.com/TomQuintana/Portfolio'
-          >
-            <h1 className='mt-8'>
-              Portfolio 
-            </h1>
-          </Link>
-          <div className='flex justify-around mt-10'>
-            <button className='w-24 p-2 bg-indigo-500 text-white uppercase rounded-xl hover:bg-indigo-600 mt-5'>
-              <Link
-                target="_blank"
-                to='https://github.com/TomQuintana/Portfolio'
-              >code
-              </Link>
-            </button>
-          </div>
-        </div>
-        <div 
+                <div 
           className="w-64 h-96 font-mono mb-16 border-2 border-gray-200 text-center shadow-lg hover:border-gray-500 hover:shadow-lg items-center"
         >
           <img
@@ -111,38 +84,6 @@ const Proyects = () => {
               <Link
                 target="_blank"
                 to='https://github.com/TomQuintana/ClimaApp.git'
-              >code
-              </Link>
-            </button>
-          </div>
-        </div>
-        <div 
-          className="w-64 h-96 font-mono mb-16 border-2 border-gray-200 text-center shadow-lg hover:border-gray-500 hover:shadow-lg items-center"
-        >
-          <img
-            className='mb-6 h-36 w-64'
-            src={logoCocch} alt="MiloComidas" />
-          <Link
-            className='text-xl hover:cursor-pointer'
-            target="_blank"
-            to='https://github.com/TomQuintana/Project-cocchautos'
-          >
-            <h1 className='mt-8'>
-              Cocchautos Detailing
-            </h1>
-          </Link>
-          <div className='flex justify-around mt-10'>
-            <button className='w-24 p-2 bg-indigo-500 text-white uppercase rounded-xl hover:bg-indigo-600 mt-5'>
-              <Link
-                target="_blank"
-                to='https://cocchautos-page.vercel.app/'
-              >website
-              </Link>
-            </button>
-            <button className='w-24 p-2 bg-indigo-500 text-white uppercase rounded-xl hover:bg-indigo-600 mt-5'>
-              <Link
-                target="_blank"
-                to='https://github.com/TomQuintana/Project-cocchautos'
               >code
               </Link>
             </button>
